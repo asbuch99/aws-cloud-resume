0233da55db00d9dab9e091e1da76f855ce5f3c25
Anvay Buch <asbuch99@gmail.com>
2024-03-27 01:11:48 -0400

    API fix

--- website/index.js
+++ website/index.js
@@ -59,9 +59,7 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
 
 const counter = document.querySelector(".counter-number");
 async function updateCounter() {
-    let response = await fetch(
-        "https://4kmsv5d2cttokhmlajz4mug5ja0eyafe.lambda-url.us-east-1.on.aws/"
-    );
+    let response = await fetch("https://4kmsv5d2cttokhmlajz4mug5ja0eyafe.lambda-url.us-east-1.on.aws/");
     let data = await response.json();
     counter.innerHTML = ` Views: ${data}`;
 }
